e0f31ee20449eb1fab6a3671fe51b9cf6e88fd5d
radenlarko <radenlarko@gmail.com>
2023-05-12 16:47:35 +0700

    Fix minor

--- src/menus/menus.module.ts
+++ src/menus/menus.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { MenusService } from './menus.service';
 import { MenusController } from './menus.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { JwtStrategy } from 'src/common/jwt.strategy';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [MenusController],
-  providers: [MenusService],
+  providers: [MenusService, JwtStrategy],
 })
 export class MenusModule {}
